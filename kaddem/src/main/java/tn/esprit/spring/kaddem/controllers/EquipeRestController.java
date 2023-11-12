4367659b0b883a00120f175c9fb286ad799069c5
KvRae <karamelmannai@gmail.com>
2023-11-12 14:41:55 +0100

    Revert to 3e0e

--- kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/EquipeRestController.java
+++ kaddem/src/main/java/tn/esprit/spring/kaddem/controllers/EquipeRestController.java
@@ -1,6 +1,8 @@
 package tn.esprit.spring.kaddem.controllers;
 
 
+import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.spring.kaddem.entities.Equipe;
 import tn.esprit.spring.kaddem.services.IEquipeService;
